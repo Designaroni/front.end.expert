fc8f4f077950e77f5fbf989b5dd081ba37a65682
Ian Roberts <ian.roberts@designaroni.com>
2020-12-19 14:59:22 -0600

    added files from initial app generation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Welcome to <a className="inline-anchor"href="https://front.end.expert/">Front<span class="typography-small-x">x</span>End<span class="typography-small-x">x</span>Expert</a></h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
