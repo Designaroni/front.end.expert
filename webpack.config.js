389fdc53c68e4202a487de3c86020a27b9c4e597
Ian Roberts <ian.roberts@designaroni.com>
2020-12-19 15:26:20 -0600

    updated package json, updated webpac config to deploy to docs instead of public

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     filename: "bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, "docs/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
